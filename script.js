309234c89a5acec55b56a07fbf89b3110d17210f
Ujjawal Mishra <themishraji93@gmail.com>
2024-10-05 18:48:12 +0530

    page2

--- script.js
+++ script.js
@@ -41,3 +41,31 @@ nav.addEventListener("mouseleave", function () {
 }
 
 navAnimation()
+
+function page2Animation(){
+    var rgihtElems = document.querySelectorAll(".right-elem")
+
+rgihtElems.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:1,
+            scale:1
+        })
+    })
+    elem.addEventListener("mouseleave",function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:0,
+            scale:0
+        })
+    })
+    elem.addEventListener("mousemove",function(dets){
+        gsap.to(elem.childNodes[3],{
+            x:dets.x - elem.getBoundingClientRect ().x-68,
+            y:dets.y - elem.getBoundingClientRect ().y-170
+        })
+    })
+})
+
+}
+
+page2Animation()
